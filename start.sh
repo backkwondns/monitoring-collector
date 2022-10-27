71e55101504a1cffef2d448f673b94e9322ec418
Hanjun Kwon <kwon96718@gmail.com>
2022-10-27 17:50:19 +0900

    fix: start.sh .env.example
    start.sh sleep을 추가
    .env.example 수정

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 docker build --tag monitoring-collector:1.0 .
-docker run -dit monitoring-collector:1.0 --name monitoring-collector
+docker run -dit --name monitoring-collector monitoring-collector:1.0
+sleep 5
 docker logs monitoring-collector
